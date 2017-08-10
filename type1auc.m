99028b6e13c5550454c6447e08e8b3285ab3c4f4
Julz <julian.r.matthews@gmail.com>
2017-08-10 18:05:05 +1000

    added dprime & criterion calculator
    Includes Hautus (1995) loglinear correction for HRs & FARs of 1 or 0

--- type1auc.m
+++ type1auc.m
@@ -8,7 +8,7 @@
 % Output is a pure frequency table and cumulative frequencies
 % Also Type-I AUC calculated using AreaUnderROC
 
-function [Frequencies, Cumulative_Frequencies, Type_One_AUC] = type1auc(signal,decision)
+function [Frequencies, Cumulative_Frequencies, Type_One_AUC,dprime,criterion] = type1auc(signal,decision)
 
 Frq = zeros(2,8);
 
@@ -56,6 +56,18 @@ Cumulative_Frequencies = Frq_Cum;
 % Calculate area under ROC curve and save as Type_One_AUC
 Type_One_AUC = AreaUnderROC([Frq_Cum(1,:);Frq_Cum(2,:)]');
 
+%% Dprime & CRITERION: ASSUMES -1 IS HIT
+% Includes loglinear correction approach from Hautus,1995
+% Add 0.5 to hits & FAs, add 1 to Signal-Presence & Signal-Absence
+
+% HITS / HITS + MISSES
+hitrate = (sum(Frequencies(1,1:4))+.5)/(sum(Frequencies(1,:))+1);
+
+% FALSE ALARMS / FALSE ALARMS + CORRECT REJECTIONS
+falserate = (sum(Frequencies(2,1:4))+.5)/(sum(Frequencies(2,:))+1);
+
+[dprime,criterion] = dprime_simple(hitrate,falserate);
+
 end
 
 %% INCLUDE AreaUnderROC FUNCTION TO MAKE PORTABLE
@@ -85,5 +97,54 @@ for i = 1:m+1
     area = area+meanhgt*width;
     totwidth = totwidth+width;
 end
+
+end
+
+function [dp,c] = dprime_simple(h,fA)
+% DPRIME_SIMPLE d' calculation given hit/false alarm rates
+%   dp = dprime_simple(h,fA) returns the d' value dp 
+%   for the hit rate h and false alarm rate fA
+%   Karin Cox 8/31/14
+%   updates:
+%   8/31/14 added criterion output c, input arg checks   
+%
+%   inputs: 
+%   h = hit rate, as float (0 < h < 1) 
+%   fA = false alarm rate, as float (0 < fA < 1)
+%
+%   outputs: 
+%   dp = d'
+%   c = criterion c (negative values --> bias towards yes responses)
+%
+%   Example:
+%   [dp,c] = dprime_simple(0.9,0.05)   
+%   dp =
+%     2.9264
+%   c = 
+%     0.1817
+%
+%   formulas: Stanislaw, H., & Todorov, N. (1999). Calculation of signal 
+%   detection theory measures. Behavior research methods, instruments, & 
+%   computers, 31(1), 137-149.
+
+
+% check n args
+narginchk(2,2);
+
+% check for values out of bounds, also issue errors or warnings if =1 or 0
+if or(or(h>1,h<0),or(fA>1,fA<0))
+    error('input arguments must fall in the 0 to 1 range')
+% standard d' formula returns NaN or Inf results for 0 or 1 inputs,
+% corrections may be required (see article above for suggestions)
+elseif or(or(h==1,h==0),or(fA==1,fA==0))
+    warning('This function will not return finite values when h or fA = 0 or 1.')
+end
+
+
+% d prime = z(h)-z(fA)
+dp = norminv(h)-norminv(fA);
+
+% c = -0.5*[z(h)+z(fA)]
+c = -0.5*(norminv(h)+ norminv(fA));
 
 end
